172298ccdd4c0e34085b4d02003685df63b09805
11florin <albuflorin929@yahoo.co.uk>
2025-07-17 13:06:17 +0100

    Updated with Constructor

--- WorldOfObjects/Main.java
+++ WorldOfObjects/Main.java
@@ -2,18 +2,18 @@ package WorldOfObjects;
 
 public class Main {
     public static void main(String[] args) {
-         Car nissan = new Car();
+         Car nissan = new Car("Nissan", 10000, 2020, "Green");
 
-        nissan.make = "Nissan";
-        nissan.price = 10000;
-        nissan.year = 2020;
-        nissan.color = "Green";
+        // nissan.make = "Nissan";
+        // nissan.price = 10000;
+        // nissan.year = 2020;
+        // nissan.color = "Green";
 
-        Car dodge = new Car();
-        dodge.make = "Dodge";
-        dodge.price = 11000;
-        dodge.year = 2019;
-        dodge.color = "Blue";
+        Car dodge = new Car("Dodge", 11000, 2019, "Blue");
+        // dodge.make = "Dodge";
+        // dodge.price = 11000;
+        // dodge.year = 2019;
+        // dodge.color = "Blue";
 
         System.out.println("This " + nissan.make + " is worth $" + nissan.price +
                 ". It was built in " + nissan.year + ". It is " + nissan.color + ".\n");
