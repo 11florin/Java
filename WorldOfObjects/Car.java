a696fae88c29d5685a72f116e970c270c13c9ee7
11florin <albuflorin929@yahoo.co.uk>
2025-08-08 14:14:59 +0100

    Adding a new method - drive()

--- WorldOfObjects/Car.java
+++ WorldOfObjects/Car.java
@@ -50,5 +50,11 @@ public class Car {
     public void setColor(String color) {
         this.color = color;
     }
+
+    public void drive() {
+        System.out.println("\nYou bought the beautiful " + this.year + " " + this.color
+         + " " + this.make + " for " + this.price + ".");
+         System.out.println("Please drive your car to the nearset exit.\n");
+    }
     
 }
