c7372470512abe2d967ecbcace21ffd960ef0d99
11florin <albuflorin929@yahoo.co.uk>
2025-08-09 15:11:49 +0100

    Workbook 7

---
+++ WorkBook/WorkBook7/Main.java
@@ -0,0 +1,13 @@
+package WorkBook.WorkBook7;
+
+public class Main {
+    public static void main(String[] args) {
+        
+        Person person = new Person("Florin", "Romanian", "01/11/1111", 5);
+
+        if (person.applyPassport()) {
+            person.setPassport();
+        }
+        System.out.println(person);
+    }
+}
