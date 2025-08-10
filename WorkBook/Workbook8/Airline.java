9447f324206f4caa1964513a688339d600d4b5a1
11florin <albuflorin929@yahoo.co.uk>
2025-08-10 18:17:14 +0100

    Workbook 8

---
+++ WorkBook/Workbook8/Airline.java
@@ -0,0 +1,25 @@
+package WorkBook.Workbook8;
+
+public class Airline {
+
+    Person[] people;
+
+    public Airline() {
+        this.people = new Person[11];
+    }
+    public Person getPerson(int index) {
+        Person person = this.people[index];
+        return new Person(person);   //copy constructor creates copy of object
+    }
+    /** Setter name: setPerson
+     * @param person (Person)
+     * 
+     * Inside the setter:
+     * 
+     *   1. Index the array, and set that element equal to the Person object. 
+     */
+    public void setPerson(Person person) {
+        int index = person.getSeatNumber() - 1;
+        this.people[index] = new Person(person);
+    }
+}
