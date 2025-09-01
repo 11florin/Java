731c37f551d94f828f8c608bf90f7489df8473e0
11florin <albuflorin929@yahoo.co.uk>
2025-09-01 17:05:58 +0100

    Extra Practice Part 3

---
+++ city/part3/CityPopulationTracker.java
@@ -0,0 +1,24 @@
+package city.part3;
+
+import java.util.HashMap;
+
+public class CityPopulationTracker {
+    
+    private HashMap<String, City> cityPopulations;
+
+    public CityPopulationTracker() {
+        this.cityPopulations = new HashMap<>();
+    }
+
+    public City getCity(String cityName) {
+        return new City(cityPopulations.get(cityName));
+    }
+
+    public void setCity(City city) {
+        this.cityPopulations.put(city.getName(), new City(city));
+    }
+
+    public void addCity(City city) {
+        this.cityPopulations.put(city.getName(), new City(city));
+    }
+}
