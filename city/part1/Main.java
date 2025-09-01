d388e51c59df23fab33f8bcd6f8a8753167476e8
11florin <albuflorin929@yahoo.co.uk>
2025-09-01 15:58:24 +0100

    Extra Practice Part 1

---
+++ city/part1/Main.java
@@ -0,0 +1,16 @@
+package city.part1;
+
+public class Main {
+
+    public static void main(String[] args) {
+        
+        City city1 = new City("New York", "Usa", 8500000);
+        System.out.println(city1.getName());
+
+        //city1.setName("");
+        //city1.setCountry("");
+        //city1.setPopulation(-1);
+
+        //City city2 = new City("", "Usa", 8500000);
+    }
+}
