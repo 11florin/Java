7a97d3386d2c7a1db09734730653365e91f2a1d0
11florin <albuflorin929@yahoo.co.uk>
2025-09-27 16:26:05 +0100

    Coding Exercise 4

---
+++ JavaOOP/classesAndObjects/exercise/codingExercise4/Cylinder.java
@@ -0,0 +1,22 @@
+package exercise.codingExercise4;
+
+public class Cylinder extends Circle {
+
+    private double height;
+
+    public Cylinder(double height, double radius) {
+        super(radius);
+        if (height < 0) {
+            this.height = 0;
+        }
+        this.height = height;
+    }
+
+    public double getHeight() {
+        return height;
+    }
+
+    public double getVolume() {
+        return getArea() * height;
+    }
+}
