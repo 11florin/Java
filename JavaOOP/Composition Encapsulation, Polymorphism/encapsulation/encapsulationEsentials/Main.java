35fdf0514167b0ca3b68b491c89054cbb98db6ff
11florin <albuflorin929@yahoo.co.uk>
2025-10-01 10:44:30 +0100

    Encapsulation Esential

--- JavaOOP/Composition Encapsulation, Polymorphism/encapsulation/encapsulationEsentials/Main.java
+++ JavaOOP/Composition Encapsulation, Polymorphism/encapsulation/encapsulationEsentials/Main.java
@@ -3,17 +3,20 @@ package encapsulation.encapsulationEsentials;
 public class Main {
     public static void main(String args[]) {
 
-        Player player = new Player();
-        player.name = "John";
-        player.health = 20;
-        player.weapon = "Sword";
+        // Player player = new Player();
+        // player.name = "John";
+        // player.health = 20;
+        // player.weapon = "Sword";
 
-        int damage = 10;
-        player.loseHealth(damage);
-        System.out.println("Remaining health: " + player.healthRemaining());
+        // int damage = 10;
+        // player.loseHealth(damage);
+        // System.out.println("Remaining health: " + player.healthRemaining());
 
-        player.loseHealth(11);
-        System.out.println("Remaining health: " + player.healthRemaining());
+        // player.loseHealth(11);
+        // System.out.println("Remaining health: " + player.healthRemaining());
+
+        EnhancedPlayer player = new EnhancedPlayer("John", 200, "Sword");
+        System.out.println("Initial health is " + player.healthRemaining()) ;
     }
 
 }
