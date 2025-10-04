7adbc01925f8fa4e8a8931092fd801a520a7d871
11florin <albuflorin929@yahoo.co.uk>
2025-10-04 13:03:48 +0100

    Introduction to Packages

---
+++ JavaOOP/Packages/src/com/abc/first/Main.java
@@ -0,0 +1,13 @@
+package com.abc.first;
+
+import com.abc.first.*;
+
+public class Main {
+    
+    public static void main(String[] args) {
+        
+        // com.abc.first.Item firstItem = new com.abc.first.Item("Burger");
+        Item firstItem = new Item("Burger");
+        System.out.println(firstItem);
+    }
+}
