f1ea26d406ac2407e372278342d1640a9844b17c
11florin <albuflorin929@yahoo.co.uk>
2025-10-07 11:58:29 +0100

    Coding Exercise 2

---
+++ JavaOOP/Arrays/ArraysProject/src/dev/codingExercise2/Main.java
@@ -0,0 +1,46 @@
+package dev.codingExercise2;
+
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class Main {
+    
+    public static void main(String[] args) {
+        
+        int readInt = readInteger();
+        System.out.println(readInt);
+
+        int[] readEl = readElements(readInt);
+        System.out.println(Arrays.toString(readEl));
+
+        int findM = findMin(readEl);
+        System.out.println("Min num = " + findM);
+    }
+    private static int readInteger() {
+
+        Scanner scan = new Scanner(System.in);
+        System.out.print("Enter count: ");
+        int count = scan.nextInt();
+        return count;
+    }
+    private static int[] readElements(int count) {
+
+        Scanner scan = new Scanner(System.in);
+        int[] array = new int[count];
+        for (int i = 0; i < array.length; i++) {
+            System.out.print("Enter a number: ");
+            int number = scan.nextInt();
+            array[i] = number;
+        }
+        return array;
+    }
+    private static int findMin(int[] array) {
+        int cmv = Integer.MAX_VALUE;
+        for (int i = 0; i < array.length; i++) {
+            if (array[i] < cmv) {
+                cmv = array[i];
+            }
+        }
+        return cmv;
+    }
+}
