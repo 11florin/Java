8b0dd5b803d0964cf23a21bdf27eb8e3d5abc8eb
11florin <albuflorin929@yahoo.co.uk>
2025-10-15 13:35:37 +0100

    autoboxing

---
+++ JavaOOP/ArrayList LinkedList and Enums/LinkedList/linkedList/src/autoboxing/Main.java
@@ -0,0 +1,31 @@
+package autoboxing;
+
+import java.util.Arrays;
+
+public class Main {
+    
+    public static void main(String[] args) {
+        
+        Integer boxedInt = Integer.valueOf(15); //prefered but unnecesary
+        Integer deprecatedBoxing = new Integer(15); //deprecated since JDK 9
+        int unboxedInt = boxedInt.intValue(); //unnecesary
+
+        //Automatic
+        Integer autoBoxed = 15;
+        int autoUnboxed = autoBoxed;
+        System.out.println(autoBoxed.getClass().getName());
+        // System.out.println(autoUnboxed.getClass().getName());
+
+        Double resultBoxed = getLiteralDoublePrimitive();
+        double resultUnboxed = getDoubleObject();
+
+        
+
+    }
+    private static Double getDoubleObject() {
+        return Double.valueOf(100.00);
+    }
+    private static double getLiteralDoublePrimitive() {
+        return 100.00;
+    }
+}
